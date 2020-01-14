10f05e4c090ade475a8bd9a56f479ee1b338d18f
AndrewSud <andrew.sudakevich@acidgreen.com.au>
2020-01-14 22:02:35 +0200

    django orm homework

--- app/concierge/mycore/admin.py
+++ app/concierge/mycore/admin.py
@@ -1,12 +1,22 @@
-# Register your models here.
-from django.conf import settings
-from django.contrib import admin
+from django.contrib.admin import ModelAdmin, register
 
-from .models import Tenant
+from .models import Tenant, Room, Journal
 
 
-@admin.register(Tenant)
-class TenantAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'date_of_birth', 'phone')
-    search_fields = ('first_name', 'last_name', 'phone', 'date_of_birth')
-    list_filter = ('date_of_birth', 'last_name')
+@register(Tenant)
+class TenantAdmin(ModelAdmin):
+    list_display = ('first_name', 'last_name')
+    search_fields = ('first_name', 'last_name')
+
+
+@register(Room)
+class RoomAdmin(ModelAdmin):
+    list_display = ('number', 'max_tenants')
+    search_fields = ('number', 'max_tenants')
+    readonly_fields = ['tenant', 'is_free']
+
+
+@register(Journal)
+class JournalAdmin(ModelAdmin):
+    list_display = ('room', 'tenant', 'tenants_count', 'check_in_date', 'check_out_date')
+    search_fields = ('room', 'tenant', 'tenants_count', 'check_in_date', 'check_out_date')
--- app/concierge/mycore/models.py
+++ app/concierge/mycore/models.py
@@ -1,54 +1,61 @@
-from django.db import models
+from django.core.exceptions import ValidationError
+from django.db.models import Model, CASCADE, IntegerField, CharField, ForeignKey, BooleanField, DateTimeField, \
+    TextField, SET_NULL
 
-# Create your models here.
+MAX_NAME_LENGTH = 80
 
-class Tenant(models.Model):
-    """
-    Room's owner/tenant
-    """
-    first_name = models.CharField(
-        'First name',
-        max_length=250,
-    )
-    last_name = models.CharField(
-        'Last name',
-        max_length=250,
-    )
-    date_of_birth = models.DateField(
-        blank=True,
-        null=True,
-        db_index=True,
-    )
-    phone = models.CharField(
-        'Phone num',
-        max_length=20,
-        blank=True,
-        null=True,
-    )
-    photo = models.ImageField(
-        'Photo',
-        upload_to='tenant',
-        help_text='Photo of the tenant',
-        null=True,
-        blank=True
-    )
-    notes = models.TextField(
-        blank=True,
-        null=True,
-    )
 
-    @property
-    def fullname(self):
-        return f'{self.first_name} {self.last_name}'
+class Tenant(Model):
+    first_name = CharField(max_length=MAX_NAME_LENGTH)
+    last_name = CharField(max_length=MAX_NAME_LENGTH)
+
+    def full_name(self):
+        vals = ' '.join([self.first_name or '', self.last_name or ''])
+        if vals.strip():
+            return vals
+        return self.pk
 
     def __str__(self):
-        return self.fullname
+        return self.full_name()
 
-    class Meta:
-        ordering = ['first_name', 'last_name']
-        indexes = [
-            models.Index(fields=['first_name', 'last_name']),
-        ]
 
-#TODO model Room
-#TODO model Journal
+class Room(Model):
+    number = IntegerField()
+    max_tenants = IntegerField('Maximum guests')
+    tenant = ForeignKey(Tenant, null=True, blank=True, on_delete=SET_NULL)
+    is_free = BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.number}'
+
+
+class Journal(Model):
+    room = ForeignKey(Room, on_delete=CASCADE)
+    tenant = ForeignKey(Tenant, on_delete=CASCADE)
+    tenants_count = IntegerField(null=True, blank=True)
+    check_in_date = DateTimeField(null=True, blank=True, db_index=True)
+    check_out_date = DateTimeField(null=True, blank=True, db_index=True)
+    notes = TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.room}, {self.tenant}, {self.tenants_count}, {self.check_in_date}, {self.check_out_date}'
+
+    def take_room(self):
+        self.room.is_free = False
+        self.room.tenant = self.tenant
+        self.room.save()
+
+    def free_room(self):
+        self.room.is_free = True
+        self.room.tenant = None
+        self.room.save()
+
+    def save(self, *args, **kwargs):
+        if self.check_out_date:
+            self.free_room()
+        else:
+            if self.tenants_count <= self.room.max_tenants:
+                self.take_room()
+            else:
+                raise ValidationError(f'Maximum count of guests {self.room.max_tenants}')
+        super().save(*args, **kwargs)
